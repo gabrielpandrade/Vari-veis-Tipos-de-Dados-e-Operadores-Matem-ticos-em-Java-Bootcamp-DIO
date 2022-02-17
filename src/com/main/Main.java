1edc70c9799b5e3922bd0a3171f81df32d78cd25
gabrielpandrade <pintoandradegabriel@gmail.com>
2022-02-17 17:29:41 -0300

    Tipos de dados

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -12,11 +12,50 @@ public class Main {
         * Parâmetro: Na assinatura do método
         * */
 
-        int Inteiro;
-        boolean Booleano;
-        float Float;
-        double Double;
-        short Short;
-        long Long;
+        byte b1 = 10;
+        byte b2 = 10;
+
+        short s1 = 10;
+        short s2 = 10;
+
+        int i1 = 10;
+        int i2 = 10;
+
+        long l1 = 10L;
+        long l2 = 10L;
+
+        float f1 = 10f;
+        float f2 = 10f;
+
+        double d1 = 10d;
+        double d2 = 10d;
+
+        char c1 = 'T';
+        char c2 = 'T';
+
+        String st1 = "Gabriel";
+        String st2 = "Gabriel";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(bo1);
+        System.out.println(bo2);
     }
 }
