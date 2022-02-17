e0487f1b28b7e9381b20f9efde854464e4c0f319
gabrielpandrade <pintoandradegabriel@gmail.com>
2022-02-17 17:17:03 -0300

    Tipos de dados

---
+++ src/com/main/Main.java
@@ -0,0 +1,22 @@
+package com.main;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        * Tipos de variáveis
+        * Instância: objeto
+        * Classe: classe
+        * Local: dentro dos métodos
+        * Parâmetro: Na assinatura do método
+        * */
+
+        int Inteiro;
+        boolean Booleano;
+        float Float;
+        double Double;
+        short Short;
+        long Long;
+    }
+}
